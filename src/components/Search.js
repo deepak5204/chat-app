0d07256d393c3141ec432435c55fa18a50ca35fd
Deepak <deepakkumar750206@gmail.com>
2022-09-06 18:32:33 +0530

    created search option

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
+    <div className='search'>
+      <div className='searchForm'>
+        <input type="text" placeholder='find a user' />
+      </div>
+      <div className='userChat'>
+        <img src='https://images.pexels.com/photos/13339064/pexels-photo-13339064.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+        <div className='userChatInfo'></div>
+        <span>Jane</span>
+      </div>
       
     </div>
   )
