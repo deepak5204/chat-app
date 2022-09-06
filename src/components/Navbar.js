02e36adb38b08f88c2904c200c7444d03f883b59
Deepak <deepakkumar750206@gmail.com>
2022-09-06 17:47:02 +0530

    created navbar in sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
+      <span className='logo'>Lama chat</span>
+      <div className='user'>
+        <img src='https://images.pexels.com/photos/13339064/pexels-photo-13339064.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
       
     </div>
   )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-     sidebar
+     <Navbar />
     </div>
   )
 }
