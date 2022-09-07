dfec5054743646daa5cb65c7e33d863712f2378c
Deepak <deepakkumar750206@gmail.com>
2022-09-07 11:01:46 +0530

    create and install firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4-AKUqHwX0AIuFYGP4j2fRAr-5sikJiA",
+  authDomain: "chat-db2f4.firebaseapp.com",
+  projectId: "chat-db2f4",
+  storageBucket: "chat-db2f4.appspot.com",
+  messagingSenderId: "917369723297",
+  appId: "1:917369723297:web:8cbaf957c6bb48fa9f13e0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
