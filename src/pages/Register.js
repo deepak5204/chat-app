3dfb797488b9ecc1037ef791e1bd9364c2b62f32
Deepak <deepakkumar750206@gmail.com>
2022-09-07 16:29:47 +0530

    ake registered user login

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,7 +4,7 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 function Register() {
   const [err, setErr] = useState(false);
@@ -22,8 +22,6 @@ function Register() {
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
       const storageRef = ref(storage, displayName);
-      console.log(displayName);
-
 
       const uploadTask = uploadBytesResumable(storageRef, file);
 
@@ -73,7 +71,7 @@ function Register() {
           <button>Sign Up</button>
           {err && <span>Something went wrong</span>}
         </form>
-        <p>You do have an account? Login</p>
+        <p>You do have an account? <Link to="/login">Login</Link></p>
       </div>
     </div>
   );
