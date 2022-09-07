bcdaf66ebae945d38e56e9e05e2999e09d4fb350
Deepak <deepakkumar750206@gmail.com>
2022-09-07 15:42:12 +0530

    user logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
+
 
 const Navbar = () => {
   return (
@@ -7,7 +10,7 @@ const Navbar = () => {
       <div className='user'>
         <img src='https://images.pexels.com/photos/13339064/pexels-photo-13339064.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
         <span>John</span>
-        <button>Logout</button>
+        <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
       
     </div>
