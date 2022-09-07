48162fb0113a26f1ecd173b89a1e3bfef76f95ad
Deepak <deepakkumar750206@gmail.com>
2022-09-07 08:11:53 +0530

    design our message content part

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      message
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img src='https://images.pexels.com/photos/13339064/pexels-photo-13339064.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img src='https://images.pexels.com/photos/13339064/pexels-photo-13339064.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+      </div>
     </div>
   )
 }
