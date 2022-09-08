6645e8c0ae0b81ee2c9e3fcd9f2a91eea63b5186
Deepak <deepakkumar750206@gmail.com>
2022-09-08 23:23:44 +0530

    improve search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,4 @@
 import React, { useContext, useState } from 'react';
-import { db } from "../firebase";
-import { AuthContext } from "../context/AuthContext";
 import {
   collection,
   query,
@@ -12,14 +10,16 @@ import {
   serverTimestamp,
   getDoc
 } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const { currentUser } = useContext(AuthContext);
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const[user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -42,16 +42,27 @@ const Search = () => {
 
   const handleSelect = async () =>{
     // check whether the group(chats in firestore) exists, if not create
-    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    const combinedId = 
+    currentUser.uid > user.uid 
+    ? currentUser.uid + user.uid 
+    : user.uid + currentUser.uid;
       try{
-        const res = await getDoc(doc(db, "chats", combinedId));
-        
+        const res = await getDoc(doc(db, "chats", combinedId));        
 
         if(!res.exists()){
           //create a chat in chats collected
           await setDoc(doc(db, "chats", combinedId), {messages: []})
 
           // create user chats
+          await updateDoc(doc(db, "userChats", currentUser.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+
           await updateDoc(doc(db, "userChats", user.uid),{
             [combinedId + ".userInfo"] : {
               uid:currentUser.uid,
@@ -59,14 +70,11 @@ const Search = () => {
               photoURL: currentUser.photoURL
             },
             [combinedId + ".date"]: serverTimestamp()
-          });
-
+          });        
         }
       }catch(err) {}
-
-
-    // create user chats
-
+      setUser()
+      setUsername("")
   }
 
   return (
@@ -74,16 +82,18 @@ const Search = () => {
       <div className='searchForm'>
         <input type="text" placeholder='Find a user'  
         onKeyDown={handleKey} 
-        onChange={(e)=>setUsername}/>
+        onChange={(e)=>setUsername(e.target.value)}/>
+        value={username}
       </div>
       {err && <span>User not found!</span>}
-      {user && <div className='userChat' onClick={handleSelect}>
-        <img src={user.photoURL} alt='' />
-        <div className='userChatInfo'>
-          <span>{user.displayName}</span>
-        </div>
-      </div>}
-      
+      {user && (
+         <div className='userChat' onClick={handleSelect}>
+         <img src={user.photoURL} alt='' />
+         <div className='userChatInfo'>
+           <span>{user.displayName}</span>
+         </div>
+       </div>
+      )}
     </div>
   )
 }
